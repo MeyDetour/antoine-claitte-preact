e59b6655ec4a051a6875a9f6af51656316cb9647
mey <meydetour@gmail.com>
2025-04-11 20:59:38 +0200

    feat: change image

--- src/data.jsx
+++ src/data.jsx
@@ -9,7 +9,7 @@ export default function projectData() {
             description: "Réalisation de la maquette Figma prototypée pour “Maison Laborde”, architecte d’intérieur",
             link: "https://www.maison-laborde.fr/",
             images: [
-                `${baseLink}/laborde/image1.png`,
+                `${baseLink}/laborde/img.png`,
                 `${baseLink}/laborde/image2.png`,
                 `${baseLink}/laborde/image3.png`,
             ]
