89de2864549b84247d54ddcef61ee899d90df031
mey <meydetour@gmail.com>
2025-04-11 20:35:48 +0200

    fix: add alt to image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ export default function ProjectsSection() {
             <div className="imgContainer">
                 {projects.map((item, i) => (
                     <a href={`/project/${item.uniqueName}`} key={i}>
-                        <img key={i} src={item.image}  alt="" />
+k                        <img key={i} src={item.image}  alt={item.title} />
                     </a>
                 ))}
             </div>
