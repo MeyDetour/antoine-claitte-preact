0c84af2a56097e16e9e441f808e0d6684a7c2fe1
mey <meydetour@gmail.com>
2025-04-11 20:34:55 +0200

    fix: add projetcs image

--- src/data.jsx
+++ src/data.jsx
@@ -13,7 +13,29 @@ export default function projectData() {
                 `${baseLink}/laborde/image2.png`,
                 `${baseLink}/laborde/image3.png`,
             ]
-        }
+        },     {
+            uniqueName: "NexPlay",
+            title: "NexPlay",
+            image: `${baseLink}/nextPlay/cover.png`,
+            description: "Réalisation de la maquette Figma prototypée ainsi que du site web sur Webflow pour un projet fictif “NexPlay”.",
+            link: "https://nexplay.webflow.io/",
+            images: [
+                `${baseLink}/nextPlay/img1.png`,
+                `${baseLink}/nextPlay/cover.png`,
+                `${baseLink}/nextPlay/img3.png`,
+            ]
+        },  {
+            uniqueName: "ChallengeBehance",
+            title: "Challenge Behance",
+            image: `${baseLink}/behance/behance.png`,
+            description: "Réalisation d’un Challenge UI qui consistait à réaliser 1 design par jour pendant 100 jours de suite.",
+            link: "https://www.behance.net/antoineclaitte",
+            images: [
+                `${baseLink}/behance/img1.png`,
+                `${baseLink}/behance/behance.png`,
+                `${baseLink}/behance/img3.png`,
+            ]
+        },
 
     ]
 
